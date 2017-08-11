6c08358c520600f0d9cb2f77d821b81ebea8ca31
dQubyte <dQubyte@gmail.com>
2017-08-11 20:59:05 +0400

    auto load language

--- install/index.php
+++ install/index.php
@@ -5,13 +5,23 @@ require_once('../admin/includes/OoTemplate.php');
 $languagesAvailableJson = file_get_contents('languages/available.json');
 $languagesAvailable = (array) json_decode($languagesAvailableJson);
 
+$languagesDefault = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+if (!in_array($languagesDefault, $languagesAvailable)){
+    $languagesDefault = 'en';
+}
+
 $languageSelect = "";
 foreach ($languagesAvailable as $key => $value) {
-	$languageSelect .= "<option>$key</option>";
+	if ($value != $languagesDefault) {
+		$languageSelect .= "<option>$key</option>";
+	} else {
+		$languageSelect .= "<option selected>$key</option>";
+	}
 }
 
 $templateData = array(
 	'languageSelect' => $languageSelect,
+	'languagesDefault' => $languagesDefault,
 	'languagesAvailableJson' => $languagesAvailableJson
 	);
 
