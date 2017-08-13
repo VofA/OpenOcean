90d067c7eea0549d8d91c4e46f22c8e7fd7c8972
dQubyte <dQubyte@gmail.com>
2017-08-13 11:19:47 +0400

    reindent

--- install/index.php
+++ install/index.php
@@ -8,7 +8,7 @@ $languagesAvailable = (array) json_decode($languagesAvailableJson);
 
 $languagesDefault = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
 if (!in_array($languagesDefault, $languagesAvailable)){
-    $languagesDefault = 'en';
+	$languagesDefault = 'en';
 }
 
 $languageSelect = "";
@@ -22,7 +22,7 @@ foreach ($languagesAvailable as $key => $value) {
 
 $templateData = array(
 	'languageSelect' => $languageSelect
-	);
+);
 
 $templateFile = file_get_contents('assets/html/index.html');
 $template = new OoTemplate($templateFile, $templateData, OO_ROOT . "install/languages/" . $languagesDefault . ".json");
